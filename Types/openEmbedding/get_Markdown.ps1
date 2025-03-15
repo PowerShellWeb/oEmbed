60aebe6332aabb578fe90a9ed9b535a3798dca8b
James Brundage <+@noreply.github.com>
2025-03-15 15:02:38 -0700

    feat: openEmbedding.get_Markdown ( Fixes #14 )
    Only linking an image if we have an image

--- Types/openEmbedding/get_Markdown.ps1
+++ Types/openEmbedding/get_Markdown.ps1
@@ -1,1 +1,6 @@
-"[![$($this.title)]($($this.thumbnail_url))]($($this.url))"
+if ($this.thumbnail_url) {
+    return "[![$($this.title)]($($this.thumbnail_url))]($($this.url))"
+}
+elseif ($this.title) {
+    "[$($this.title)]($($this.url))"
+}
